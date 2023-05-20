15ab59b6d06aa5a91756368bea9b842e33503a64
“AmAn-2O” <amandeep700933@gmail.com>
2023-05-20 14:56:13 +0530

    third commit

--- infy.py
+++ infy.py
@@ -1,13 +1,13 @@
-# count=0
-# i=1
-# for baggage_weight in 29, 30, 31, 32, 28:
-#     if(baggage_weight>=1 and baggage_weight <=30):
-#         print("Passenger",i,": Proceed for baggage check.")
-#         count+=1
-#     else:
-#         print("Passenger",i,": Maximum baggage weight allowed is 30kg.")
-#     i+=1
-# print("No. of passengers who cleared baggage check:", count)
+count=0
+i=1
+for baggage_weight in 29, 30, 31, 32, 28:
+    if(baggage_weight>=1 and baggage_weight <=30):
+        print("Passenger",i,": Proceed for baggage check.")
+        count+=1
+    else:
+        print("Passenger",i,": Maximum baggage weight allowed is 30kg.")
+    i+=1
+print("No. of passengers who cleared baggage check:", count)
 
 i=1
 n=10
@@ -15,3 +15,5 @@ while(i<=n):
     if(i%2==0):
         print(i)
     i+=1
+
+print("Welcome to the world of web3")
